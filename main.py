201390ef8702a124caf7ea3169cb5b326fa360e4
Carl Beekhuizen <carl.beek@gmail.com>
2019-06-12 17:51:35 -0400

    Before the fuckening

--- main.py
+++ main.py
@@ -5,20 +5,16 @@ class Block:
 
 
 class Node:
-    def __init__(self, block, parent, is_latest, score=0, children=None, child_block_to_child=None):
+    def __init__(self, block, parent, is_latest, score=0, children=None):
         if children is None:
             self.children = set()
-            self.child_block_to_child = dict()
         else:
             self.children = children
-            self.child_block_to_child = child_block_to_child
-
         self.block = block
         self.parent = parent
         self.is_latest = is_latest
         self.score = score
 
-        
 
     def size(self):
         size = 1
@@ -52,34 +48,24 @@ class CompressedTree:
                 return node
         return None
 
-    # returns both the first node in the tree found behind the prevblock pointer of block
-    # and the child block of that nodes' block with which it agrees
     def find_prev_in_tree(self, block):
         # Return the node, that contains the block, that is the highest block that is a previous block of block
 
-        prev_block = block
         curr = block
         while not self.block_in_tree(curr):
-            prev_block = curr
             curr = curr.parent_block
 
         # get the node that contains this block
-        return self.node_with_block(curr, self.root), prev_block
+        return self.node_with_block(curr, self.root)
 
-    # returns the "latest common ancestor" or latest agreed upon block between blocks b1 and b2
-    # and also the children of that block from the chains of the two b1 and b2, respectively
     def find_lca(self, block_1, block_2):
-        prev_block_1 = block_1
         curr_block_1 = block_1
         while curr_block_1 is not None:
-            prev_block_2 = block_2
             curr_block_2 = block_2
             while curr_block_2 is not None:
                 if curr_block_1 == curr_block_2:
-                    return curr_block_1, prev_block_1, prev_block_2
-                prev_block_2 = curr_block_2
+                    return curr_block_1
                 curr_block_2 = curr_block_2.parent_block
-            prev_block_1 = curr_block_1
             curr_block_1 = curr_block_1.parent_block
             
         raise AssertionError("Fuuuuuck")
@@ -95,25 +81,22 @@ class CompressedTree:
         return new_node
 
     def add_block(self, block):
-        prev_in_tree, pit_child_block = self.find_prev_in_tree(block)
+        prev_in_tree = self.find_prev_in_tree(block)
         if len(prev_in_tree.children) == 0:
             node = Node(block, prev_in_tree, is_latest=True)
             prev_in_tree.children.add(node)
-            prev_in_tree.child_block_to_child[node] = pit_child_block
             return node
-        if pit_child_block in prev_in_tree.child_block_to_child:
-            ancestor, blocks_child_of_ancestor, pit_child_of_ancestor = self.find_lca(block, prev_in_tree.child_block_to_child[pit_child_block])
-            node = Node(block, ancestor, is_latest=True)
-            anc_node = Node(ancestor, prev_in_tree, children={node, child}, is_latest=False, child_block_to_child={pit_child_of_ancestor : child, blocks_child_of_ancestor : node})
-            prev_in_tree.children.add(anc_node)
-            prev_in_tree.child_block_to_child[pit_child_block] = anc_node
-            prev_in_tree.children.remove(child)
-            return node
+        for child in prev_in_tree.children:
+            ancestor = self.find_lca(block, child.block)
+            if ancestor != prev_in_tree.block:
+                node = Node(block, ancestor, is_latest=True)
+                anc_node = Node(ancestor, prev_in_tree, children={node, child}, is_latest=False)
+                prev_in_tree.children.add(anc_node)
+                prev_in_tree.children.remove(child)
+                return node
         # insert on the prev_in_tree
         node = Node(block, prev_in_tree, True)
         prev_in_tree.children.add(node)
-        prev_in_tree.child_block_to_child[node] = pit_child_block
-        
         return node
 
     def size(self):
